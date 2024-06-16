ff89a3b4854e05b305f04c9609c1ca3004c6e1d9
Neuths <117487287+Neuths@users.noreply.github.com>
2024-06-16 16:43:38 +0300

    a lot of comments to .js

--- merge3/merge3.js
+++ merge3/merge3.js
@@ -28,10 +28,10 @@ function setGame() { // prepares the game board
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             //<div id="0-0"></div>
-            let tile = document.createElement("div");
+            let tile = document.createElement("div"); // creates 25 id's so that each square has an id, done with looping
             tile.id = r.toString() + "-" + c.toString();
             let num = board[r][c];
-            updateTile(tile, num);
+            updateTile(tile, num); // updates the tile, gets looped
             document.getElementById("board").append(tile);
         }
     }
@@ -39,7 +39,7 @@ function setGame() { // prepares the game board
     setNew();
 }
 
-function setNew() { // sets a new tile on board
+function setNew() { // borrowed - randomly sets a new tile on board
     let found = false;
     while (!found) {
         let r = Math.floor(Math.random() * rows);
@@ -55,7 +55,7 @@ function setNew() { // sets a new tile on board
     }
 }
 
-function updateTile(tile, num) { // updates tile visually in HTML
+function updateTile(tile, num) { // borrowed - updates tile visually in HTML
     tile.innerText = "";
     tile.classList.value = ""; //clear the classList
     tile.classList.add("tile");
@@ -65,26 +65,26 @@ function updateTile(tile, num) { // updates tile visually in HTML
         if (num <= 12) {
             tile.classList.add("x"+num.toString());
         } else {
-            tile.classList.add("x13");
+            tile.classList.add("x13"); // doesn't change tile style after 13th tile
         }
     }
 }
 
 
-function merge() {
+function merge() {  // giant pyramid just for the delay, otherwise you cannot add delay between the merges properly (since it is very hard to stop JavaScript code from running)
     mergeDelay = false;
     scoreCheck = score;
-    h5();
-    if (mergeDelay == false) {
-        v5();
+    v5();
+    if (mergeDelay == false) { // makes sure that the function is restarted with delay after a merge happens
+        h5();
         if (mergeDelay == false) {
-            h4();
+            v4();
             if (mergeDelay == false) {
-                v4();
+                h4();
                 if (mergeDelay == false) {
-                    h3();
+                    v3();
                     if (mergeDelay == false) {
-                        v3();
+                        h3();
                         if (scoreCheck != score) {
                             wait();
                         }
@@ -105,44 +105,44 @@ function merge() {
     }
     
     if (scoreCheck < score) {
-        shouldSet = false;
+        shouldSet = false; // if a merge has happened, don't set a new tile
     }
 
     for (let r = 0; r < rows; r++) { // update all tiles after merging
         for (let c = 0; c < columns; c++) {
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             let num = board[r][c];
-            updateTile(tile, num);
+            updateTile(tile, num); // updates all the tiles after merging
         }
     }
 }
 
 
-function wait() {
+function wait() { // attempt at making the game have delay when merging
     setTimeout(merge, 300);
 }
 
-
+// functions h5 to v3 check for different types of merges, they needed to be separated so that they don't run all at once and merge delay can be added
 function h5() { // 5 horizontal
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns - 4; c++) {
-            let tile1 = board[r][c];
+            let tile1 = board[r][c]; // checks 5 tiles in a row for merging
             let tile2 = board[r][c+1];
             let tile3 = board[r][c+2];
             let tile4 = board[r][c+3];
             let tile5 = board[r][c+4];
-            if (tile1 != 0 && tile1 == tile2 && tile2 == tile3 && tile3 == tile4 && tile4 == tile5) {
+            if (tile1 != 0 && tile1 == tile2 && tile2 == tile3 && tile3 == tile4 && tile4 == tile5) { // if 5 tiles in a row are same and not 0
 
-                console.log("merged 5 " + tile3 + "'s horizontally");
+                console.log("merged 5 " + tile3 + "'s horizontally"); // concole.log tracking
                 console.log("Added " + (tile3 * 5) + " points!");
-            
-                board[r][c] = 0;
+                
+                board[r][c] = 0; // how game handles merging, for 5-in-a-row it upgrades the middle tile twice
                 board[r][c+1] = 0;
                 board[r][c+2] += 2;
                 board[r][c+3] = 0;
                 board[r][c+4] = 0;
-                score += (tile3 * 5);
-                mergeDelay = true;
+                score += (tile3 * 5); // adds points to score
+                mergeDelay = true; // a merge has happened, so it will delay the next merge
             }
         }
     }
@@ -261,7 +261,7 @@ function v3() { // 3 vertical
     }
 }
 
-function slide(row) { // prepares the direction of the slide
+function slide(row) { // prepares the direction of the slide, taking all zeroes and moving them in the opppsite direction
     for (i = 1; i < row.length; i++) {
       if (row[i-1] == 0) {
         row[i-1] = row[i];
@@ -275,7 +275,7 @@ function slide(row) { // prepares the direction of the slide
 function slideLeft() {
     for(let r = 0; r < rows; r++) {
         let row = board[r];
-        row = slide(row);
+        row = slide(row); // prepares row
         board[r] = row;
 
         for (let c = 0; c < columns; c++) {
@@ -290,7 +290,7 @@ function slideLeft() {
 function slideRight() {
     for(let r = 0; r < rows; r++) {
         let row = board[r];
-        row.reverse();
+        row.reverse(); // does the same as slideLeft() but reverses the row twice, once before moving the row and once after so that the same function can be used
         row = slide(row);
         row.reverse();
         board[r] = row;
@@ -307,7 +307,7 @@ function slideUp() {
     for(let c = 0; c < columns; c++) {
         let row = [board[0][c], board[1][c], board[2][c], board[3][c], board[4][c]];
         row = slide(row);
-        board[0][c] = row[0];
+        board[0][c] = row[0]; // for up and down the rows need to be made vertical, otherwise same logic
         board[1][c] = row[1];
         board[2][c] = row[2];
         board[3][c] = row[3];
@@ -342,7 +342,7 @@ function slideDown() {
 }
 
 
-document.addEventListener("keyup", (e) => { // takes the arrow key presses
+document.addEventListener("keyup", (e) => { // top part borrowed - takes the arrow key presses
     console.log("move");
     let move = false;
     shouldSet = true;
@@ -384,13 +384,13 @@ document.addEventListener("keyup", (e) => { // takes the arrow key presses
         slideDown();
     }
 
-    merge();
+    merge(); // if a merge happens it runs itself again until no merges happen
 
-    if (move == true && shouldSet == true) {
-        setNew();
-        shouldSet = false;
+    if (move == true && shouldSet == true) { // checks if any of the tiles moved after keypress, and if a merge has happened
+        setNew(); // new tile
+        shouldSet = false; // reset setting
         wait();
     }
 
-    document.getElementById("score").innerText = score;
+    document.getElementById("score").innerText = score; // update score
 })
